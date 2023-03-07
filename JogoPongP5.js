5aa1d1700904342be6004ab9d1c4eaac69730ccb
PedroDiOliveira <126473107+PedroDiOliveira@users.noreply.github.com>
2023-03-07 19:54:25 -0300

    Update JogoPongP5.js

--- JogoPongP5.js
+++ JogoPongP5.js
@@ -4,6 +4,16 @@ let yBolinha = 200;
 let diametro = 20;
 let raio = diametro / 2;
 
+//Variaveis da Raquete
+let xRaquete = 5;
+let yRaquete = 150;
+let larguraRaquete = 10;
+let alturaRaquete = 90;
+
+//Variaveis do Oponente
+let xOponente = 585
+let yOponente = 150
+
 //Velocidade da Bolinha
 let velocidadeXBolinha = 6;
 let velocidadeYBolinha = 2;
@@ -14,9 +24,16 @@ function setup() {
 
 function draw() {
   background(0);
-mostraBolinha();
-movimentaBolinha();
-verificaColisaoBorda(); 
+//Bolinha
+  mostraBolinha();
+  movimentaBolinha();
+  verificaColisaoBorda();
+  
+//Raquete
+  mostraRaquete();
+  
+//Oponente
+  mostraOponente();  
 }
 
 function mostraBolinha(){
@@ -31,11 +48,16 @@ yBolinha += velocidadeYBolinha
 function verificaColisaoBorda(){
   if(xBolinha + raio > width || xBolinha -raio < 0){
 velocidadeXBolinha *= -1
-} ;
+} 
   
-
   if(yBolinha + raio > height ||
      yBolinha - raio < 0){
 velocidadeYBolinha *= -1  
-  };
+  }
+}
+function mostraRaquete(){
+  rect(xRaquete, yRaquete, larguraRaquete, alturaRaquete)
+}
+function mostraOponente(){
+  rect(xOponente, yOponente, larguraRaquete, alturaRaquete)
 }
