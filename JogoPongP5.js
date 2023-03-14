73e6c1718421da5789d471e57476fe30f77a5752
PedroDiOliveira <126473107+PedroDiOliveira@users.noreply.github.com>
2023-03-14 19:51:03 -0300

    Update JogoPongP5.js

--- JogoPongP5.js
+++ JogoPongP5.js
@@ -3,53 +3,44 @@ let xBolinha = 300;
 let yBolinha = 200;
 let diametro = 20;
 let raio = diametro / 2;
-
 //Variaveis da Raquete
 let xRaquete = 5;
 let yRaquete = 150;
 let larguraRaquete = 10;
 let alturaRaquete = 90;
-
 //Variaveis do Oponente
 let xOponente = 585
 let yOponente = 150
-
 //Velocidade da Bolinha
 let velocidadeXBolinha = 6;
 let velocidadeYBolinha = 2;
-
 function setup() {
   createCanvas(600, 400);
 }
-
 function draw() {
   background(0);
 //Bolinha
   mostraBolinha();
   movimentaBolinha();
-  verificaColisaoBorda();
-  
-//Raquete
+  verificaColisaoBorda();  
+  verificaColisaoRaquete();
+  //Raquete
   mostraRaquete();
-  
+  movimentaMinhaRaquete();
 //Oponente
   mostraOponente();  
 }
-
 function mostraBolinha(){
 circle (xBolinha,yBolinha,diametro)
 }
-
 function movimentaBolinha(){
 xBolinha += velocidadeXBolinha
 yBolinha += velocidadeYBolinha 
 }
-
 function verificaColisaoBorda(){
   if(xBolinha + raio > width || xBolinha -raio < 0){
 velocidadeXBolinha *= -1
-} 
-  
+}   
   if(yBolinha + raio > height ||
      yBolinha - raio < 0){
 velocidadeYBolinha *= -1  
@@ -60,4 +51,17 @@ function mostraRaquete(){
 }
 function mostraOponente(){
   rect(xOponente, yOponente, larguraRaquete, alturaRaquete)
+}
+function movimentaMinhaRaquete() {
+    if (keyIsDown(UP_ARROW)) {
+        yRaquete -= 10;
+    }
+    if (keyIsDown(DOWN_ARROW)) {
+        yRaquete += 10;
+    }
+}
+function verificaColisaoRaquete() {
+    if (xBolinha - raio < xRaquete + larguraRaquete && yBolinha - raio < yRaquete + alturaRaquete && yBolinha + raio > yRaquete) {
+        velocidadeXBolinha *= -1;
+    }
 }
